e3e18d5aa4a513c34492a66cd819e52318eecbd4
Elsa Rhudye Mangoye <diamant.rhudye@gmail.com>
2021-07-01 15:00:45 +0200

    creer la route dans videocontroller et ajouter le fichier add dans le template

--- src/Controller/VideoController.php
+++ src/Controller/VideoController.php
@@ -2,16 +2,38 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Video;
+use App\Form\VideoType;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 
 class VideoController extends AbstractController
 {
-    #[Route('/video', name: 'video')]
+    #[Route('/', name: 'app_home')]
     public function index(): Response
     {
         return $this->render('video/index.html.twig', [
+            'controller_name' => 'VideoController',
+        ]);
+    }
+
+    #[Route('/video/add', name: 'video_add')]
+    public function add(Request $request): Response
+    {
+
+        $video = new Video();
+        $form = $this->createForm(VideoType::class, $video);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($video);
+            $em->flush();
+            return $this->redirectToRoute('app_home');
+        }
+        return $this->render('video/add.html.twig', [
             'controller_name' => 'VideoController',
         ]);
     }
